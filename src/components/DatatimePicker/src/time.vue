e74e26a85783615c9d755aac1d6c1a18ddba38cb
Middletwo-Kid <976989701@qq.com>
2021-04-03 18:47:05 +0800

    fix: 修复时间显示

--- src/components/DatatimePicker/src/time.vue
+++ src/components/DatatimePicker/src/time.vue
@@ -35,18 +35,30 @@ export default {
     reset() {
       this.index = '';
     },
+    initData(val) {
+      if (val && val.length > 0) {
+        switch (this.type) {
+          case 'year': return val.slice(0, 4);
+          case 'month': return val.slice(0, 6);
+          case 'day': return val;
+          default: return '';
+        }
+      } else {
+        return '';
+      }
+    },
   },
   watch: {
     startTime: {
       immediate: true,
       handler(newVal) {
-        this.firstValue = this.type === 'month' && newVal && newVal.length > 0 ? newVal.slice(0, 6) : newVal;
+        this.firstValue = this.initData(newVal);
       },
     },
     endTime: {
       immediate: true,
       handler(newVal) {
-        this.secondValue = this.type === 'month' && newVal && newVal.length > 0 ? newVal.slice(0, 6) : newVal;
+        this.secondValue = this.initData(newVal);
       },
     },
   },
